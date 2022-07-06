5ccc08e3d4644279e46e6fdd2510ec5bb20760f0
Jorge <fulano@email.com>
2022-07-06 12:18:25 -0300

    comecando a codar

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import router from './routes/index.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.use(router);
+
+server.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
+});
