315c66fc1dbf4eafdecdadd494c0b60c5f99c150
JorgeMalaquias <74991858+JorgeMalaquias@users.noreply.github.com>
2022-07-11 16:04:24 -0300

    'deployando'

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,9 +1,12 @@
 import { gettingProducts } from '../controllers/productController.js';
 import { gettingOneProduct } from '../controllers/productController.js';
 import { postPurchase } from '../controllers/cartController.js';
+import { userValidate } from '../middlewares/productsMiddleware.js';
 import { Router } from 'express';
 
 const router = Router();
+
+
 
 router.get('/products', gettingProducts);
 router.get('/products/:id',gettingOneProduct);
