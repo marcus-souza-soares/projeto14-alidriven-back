a56002fd59567766b2fd58e02fb927e3ed13d28c
Marcus <marcusadrenaline@gmail.com>
2022-07-11 18:26:05 -0300

    make it better

--- src/db.js
+++ src/db.js
@@ -6,5 +6,5 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 await mongoClient.connect();
 
-const db = mongoClient.db("alidriven");
+const db = mongoClient.db("Alidriven");
 export default db;
